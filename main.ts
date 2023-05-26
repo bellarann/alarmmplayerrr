f157fda8cf42044354a1e4f7cd5a59b78f42a31a
bellarann <131433170+bellarann@users.noreply.github.com>
2023-05-26 21:45:46 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+    if (true) {
+        for (let index = 0; index < 5; index++) {
+            music.playMelody("B E B E B E B E ", 296)
+        }
+    } else {
+        basic.clearScreen()
+    }
+})
+radio.setGroup(8148)
